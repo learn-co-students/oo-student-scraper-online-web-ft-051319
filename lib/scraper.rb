e4ee41707be97f509d2ec50d86be42fbd0f4bee9
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 13:19:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,36 +7,18 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-# <<<<<<< HEAD
 
-    students_hash = {}
+
 
     doc.css(".student-card").map do |student|
-      students_hash[:name] = student.css("h4").text,
-      students_hash[:location] = student.css("p").text,
-      students_hash[:profile_url] = student.css('href').text
-      binding.pry
+      students_hash = {}
+      students_hash[:name] = student.css("h4.student-name").text
+      students_hash[:location] = student.css("p").text
+      students_hash[:profile_url] = student.css("a").attr('href').text
+      students_hash
     end
 
 
-# =======
-# >>>>>>> fbbbf0054a3755e90937e7eb94558c58212aaa77
-
-    students_hash = {}
-
-    doc.css(".student-card").map do |student|
-      students_hash[:name] = student.css("h4.student-name").text,
-      students_hash[:location] = student.css("p").text,
-      students_hash[:profile_url] = student.css('href').text
-
-    end
-
-#
-# <<<<<<< HEAD
-# =======
-#
-#
-# >>>>>>> fbbbf0054a3755e90937e7eb94558c58212aaa77
     # doc.css(".student-card").first
     #student_name = doc.css(".student-card").first.css("h4").text
     #student_location = doc.css(".student-card").first.css("p").text
@@ -49,7 +31,6 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    binding.pry
 
     # twitter url:
     # linkedin url:
