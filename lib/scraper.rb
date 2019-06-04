f9ce9537d969bdab30722c8df3fe50ef434ad1eb
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 14:22:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,14 +9,16 @@ class Scraper
     doc = Nokogiri::HTML(html)
 
 
-
+    students = []
     doc.css(".student-card").map do |student|
       students_hash = {}
       students_hash[:name] = student.css("h4.student-name").text
       students_hash[:location] = student.css("p").text
       students_hash[:profile_url] = student.css("a").attr('href').text
-      students_hash
+      students << students_hash
     end
+    students
+
 
 
     # doc.css(".student-card").first
@@ -31,24 +33,36 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    # twitter = doc.css(".social-icon-container").css("a")[0].attr("href")
-    # linkedin =  doc.css(".social-icon-container").css("a")[1].attr("href")
-    # github =  doc.css(".social-icon-container").css("a")[2].attr("href")
-    # blog url =  doc.css(".social-icon-container").css("a")[3].attr("href")
-    # profile quote = doc.css(".vitals-text-container .profile-quote").text
-
-
-
-    binding.pry
-
-    # twitter url:
-    # linkedin url:
-    # github url:
-    # blog url:
-    # profile quote:
-    # bio:
-
+    attributes = {}
+    doc.css("div.social-icon-container a").each do |social_media|
+      case social_media
+      when /twitter/
+        attributes[:twitter] = social_media.attribute("href").value
+      when /linkedin/
+        attributes[:linkedin] = social_media.attribute("href").value
+      when /github/
+        attributes[:github] = social_media.attribute("href").value
+      else
+        attributes[:blog] = social_media.attribute("href").value
+      end
+      attributes[:profile_quote] = doc.css("div.profile-quote").text
+      attributes[:bio] = doc.css("div.bio-content div.description-holder p").text.strip
+    end
+    attributes
   end
+      # twitter url: doc.css("div.social-icon-container a").attr("href").value
+    # linkedin url: doc.css("div.social-icon-container a")attr("href").value
+    # github url:  doc.css("div.social-icon-container a").attr("href").value
+    # blog url : doc.css("div.social-icon-container a").attr("href").value
+    # profile quote :doc.css("div.profile-quote").text
+    #bio : doc.css("div.description-holder p").text
 
 
+
+
+
+
+
+
+    Scraper.scrape_profile_page("./fixtures/student-site/students/joe-burgess.html")
 end
