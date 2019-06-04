55cddd7446f245011721527510fd8efcb86be639
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 04:00:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,13 @@ class Scraper
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     binding.pry
+
+
     # students_hash = {}
-    #
+    #doc.css(".student-card").first
+    #student_name = doc.css(".student-card").first.css("h4").text
+    #student_location = doc.css(".student-card").first.css("p").text
+    #student_URL = doc.css(".student-card a").attr('href').text
     end
 
     Scraper.scrape_index_page("./fixtures/student-site/index.html")
