1d56e490da5e810f2e143a432b77c88146cf22a5
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 11:36:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,20 +7,41 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    binding.pry
+
+    students_hash = {}
+
+    doc.css(".student-card").map do |student|
+      students_hash[:name] = student.css("h4").text,
+      students_hash[:location] = student.css("p").text,
+      students_hash[:profile_url] = student.css('href').text
+      binding.pry
+    end
 
 
-    # students_hash = {}
-    #doc.css(".student-card").first
+
+
+    # doc.css(".student-card").first
     #student_name = doc.css(".student-card").first.css("h4").text
     #student_location = doc.css(".student-card").first.css("p").text
     #student_URL = doc.css(".student-card a").attr('href').text
-    end
+  end
 
     Scraper.scrape_index_page("./fixtures/student-site/index.html")
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
+
+    # twitter url:
+    # linkedin url:
+    # github url:
+    # blog url:
+    # profile quote:
+    # bio:
 
   end
+  Scraper.scrape_index_page("./fixtures/student-site/students/eric-chu.html")
+
 
 end
