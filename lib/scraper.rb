1dc643ed71df7f774cbfba386273f7401919b925
Alethea Cary <aletheacary@gmail.com>
2019-06-09 22:43:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,18 +17,29 @@ class Scraper
   
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    doc.css("div.social-icon-container a img")each_with_index
+    
+    student_info = {}
+    
+    link = doc.css("div.social-icon-container a").attribute("href").value
+    if link.include?("twitter") == true
+      student_info[:twitter] = link
+    elsif link.include?("linkedin") == true
+      student_info[:linkedin] = link
+    elsif link.include?("github") == true
+      student_info[:github] = link
+    else
+      student_info[:blog] = link
+    end
+      
+   
+    # doc.css("div.social-icon-container a img")each_with_index
+    
+    
+    
+    # if doc.css("div.social-icon-container a img") include?(http://178.128.14.28:58884/fixtures/student-site/assets/img/twitter-icon.png)
+    # end
     
    
-    # twitter_icon = http://178.128.14.28:58884/fixtures/student-site/assets/img/twitter-icon.png
-    # linkedin-icon = http://178.128.14.28:58884/fixtures/student-site/assets/img/linkedin-icon.png
-    # github-icon = http://178.128.14.28:58884/fixtures/student-site/assets/img/github-icon.png
-    #
-    doc.css(div.social-icon-container)
-    if doc.css("div.social-icon-container a img") include?(http://178.128.14.28:58884/fixtures/student-site/assets/img/twitter-icon.png)
-    end
-    
-    # student_info = {}
     # student_info[:linkedin] = doc.css()
     # student_info[:github] = 
     # student_info[:blog] = 
@@ -40,3 +51,4 @@ class Scraper
 
 end
 
+# doc.css("div.social-icon-container a").attribute("href").value 
